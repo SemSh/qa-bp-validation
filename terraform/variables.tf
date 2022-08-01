ab0aded4a5a87e189088b156a05145308a53ae17
asafyahalom <49859118+asafyahalom@users.noreply.github.com>
2022-08-01 14:34:01 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,3 +25,7 @@ variable "user" {
     description = "Username to assign permissions for S3 bucket to. If left blank, will not create permissions."
     default = "none"
 }
+
+variable "test" {
+    description = "Double slash test"
+}
